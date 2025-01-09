4817ac9d2709b6479ca39a8df88e37710cd34247
zacw-243L <qwerty.amousy123442x@gmail.com>
2025-01-09 11:02:48 +0800

    You now have games

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import '../screens/login_screen.dart';
 import '../screens/home_screen.dart';
 import '../screens/exercise_screen.dart';
 import '../screens/update_fitness_user_screen.dart';
+import '../screens/game.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
