dcbb3eea3063a4521391a1eb0f2e7e72024abc8d
juliusthecoder <julius.sim@live.com>
2025-01-11 01:13:16 +0800

    completion of api_calls.dart
    need more work for home_screen.dart, planning to implement a listview.builder inside futurebuilder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../utilities/firebase_calls.dart';
-// import '../utilities/api_calls.dart';
 import '../widgets/navigation_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -30,28 +29,10 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       bottomNavigationBar: MyBottomNavigationBar(selectedIndexNavBar: 0),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Text('Welcome back, ${auth.currentUser?.displayName}.'),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-            ),
-            ListView.builder(
-              itemCount: Bmidata.length,
-              itemBuilder: (context, index) {
-                // FutureBuilder<Bmi>(
-                //   future: fetchBmi(Bmidata),
-                //   builder: (context, snapshot) {
-                //
-                //   }
-                // ),
-              },
-            ),
-            //TODO widget to show show bmi, bmiConclusion, ideal body weight, body fat and daily energy expenditure
-          ],
-        ),
-      ),
+      // body: SafeArea(
+      //   child: FutureBuilder<List<Bmi>>(future: fetchBmi(), builder: (context)),
+      //   //TO DO widget to show show bmi, bmiConclusion, ideal body weight, body fat and daily energy expenditure
+      // ),
     );
   }
 }
--- lib/utilities/api_calls.dart
+++ lib/utilities/api_calls.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
-// import '../models/bmi.dart';
+import '../models/bmi.dart';
 
 class ApiCalls {
-  void fetchBmi() async {
-    String baseURL = 'https://fitness-api.p.rapidapi.com/fitness';
+  Future<List<Bmi>> fetchBmi(String baseURL) async {
+    final String baseURL = 'https://fitness-api.p.rapidapi.com/fitness';
+    MapEntry<String, String> EE_Entry =
+        MapEntry("totalDailyEnergyExpenditure.bmi.calories", "value");
 
     Map<String, String> requestHeaders = {
       'X-RapidAPI-Host': 'fitness-api.p.rapidapi.com',
@@ -13,10 +17,10 @@ class ApiCalls {
     };
 
     Map<String, String> payload = {
-      'X-RapidAPI-Host': 'fitness-api.p.rapidapi.com',
-      'X-RapidAPI-Key': 'c93f0e349bmsh4f90f71e75907a8p166d42jsne574e84b3e47',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      //Add API request parameters
+      "bodyMassIndex": "conclusion, value",
+      "idealBodyWeight.peterson.metric": "value",
+      "bodyFatPercentage.bmi": "value",
+      EE_Entry.key: EE_Entry.value,
     };
 
     var request = http.Request('POST', Uri.parse(baseURL));
@@ -26,10 +30,12 @@ class ApiCalls {
     http.StreamedResponse response = await request.send();
 
     if (response.statusCode == 200) {
-      // List<dynamic> jsonList = jsonDecode(response.body) as List<dynamic>;
-      // List<Bmi> BmiInfo = jsonList.map((json) => Bmi.fromJson(json)).toList();
-      // return BmiInfo; //uncomment when apis are implemented in their respective pages
-      // //TODO return Bmi object
+      String ReceivedResponse = await response.stream.bytesToString();
+      List<dynamic> jsonList = jsonDecode(ReceivedResponse) as List<dynamic>;
+
+      List<Bmi> bmiInfo = jsonList.map((json) => Bmi.fromJson(json)).toList();
+      return bmiInfo; //uncomment when apis are implemented in their respective pages
+      //TODO return Bmi object
     } else {
       throw Exception('Failed to load bmi');
     }
