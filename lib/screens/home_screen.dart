9796488463666464572202f489b20013c393e2e0
juliusthecoder <julius.sim@live.com>
2025-01-13 00:55:34 +0800

    Near completion of home_screen.dart
    left the user authentication part whereby they greet the user when he/she logs in, roy shall do firebase calls which will involve this part too

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/bmi.dart';
+import '../utilities/api_calls.dart';
 import '../utilities/firebase_calls.dart';
 import '../widgets/navigation_bar.dart';
 
@@ -11,8 +13,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+<<<<<<< HEAD
   List<String> Bmidata = [];
+  late Future<List<Bmi>> BmiFS;
 
+  @override
+  void initState() {
+    super.initState();
+    ApiCalls apiCalls = ApiCalls(); // Create an instance of ApiCalls
+    BmiFS = apiCalls.fetchBmi(
+        'https://fitness-api.p.rapidapi.com/fitness'); // Call fetchBmi
+  }
+
+=======
+>>>>>>> 0a7a60136f2d78c788e6765098884e9d16dbfed2
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,11 +42,59 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
+      body: SafeArea(
+        child: FutureBuilder<List<Bmi>>(
+          future: BmiFS,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  Bmi someBmiData = snapshot.data![index];
+                  return Card(
+                    margin: EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(
+                          'Welcome Adam'), //need to implement firebase authentication here to display user name
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                              'Your BMI is ${someBmiData.bmi.toStringAsFixed(1)}'),
+                          Text('You are ${someBmiData.bmiConclusion}'),
+                          Text(
+                              'Ideal body weight is ${someBmiData.idealBodyWt.toStringAsFixed(1)} kg'),
+                          Text(
+                              'Body Fat is ${someBmiData.bodyFatPercent.toStringAsFixed(1)}%'),
+                          Text(
+                              'Total Daily Energy Expenditure is ${someBmiData.totalDailyEE} kcals'),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+            return const CircularProgressIndicator();
+          },
+        ),
+        //TODO widget to show show bmi, bmiConclusion, ideal body weight, body fat and daily energy expenditure
+      ),
       bottomNavigationBar: MyBottomNavigationBar(selectedIndexNavBar: 0),
-      // body: SafeArea(
-      //   child: FutureBuilder<List<Bmi>>(future: fetchBmi(), builder: (context)),
-      //   //TO DO widget to show show bmi, bmiConclusion, ideal body weight, body fat and daily energy expenditure
-      // ),
+<<<<<<< HEAD
+=======
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text('Welcome ${auth.currentUser?.displayName}'),
+            //TODO widget to show show bmi, bmiConclusion, ideal body weight, body fat and daily energy expenditure
+          ],
+        ),
+      ),
+>>>>>>> 0a7a60136f2d78c788e6765098884e9d16dbfed2
     );
   }
 }
--- lib/utilities/api_calls.dart
+++ lib/utilities/api_calls.dart
@@ -1,26 +1,27 @@
+import '../models/fitness_user.dart';
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
-import '../models/bmi.dart';
-
 class ApiCalls {
+<<<<<<< HEAD
   Future<List<Bmi>> fetchBmi(String baseURL) async {
-    final String baseURL = 'https://fitness-api.p.rapidapi.com/fitness';
+    const String baseURL = 'https://fitness-api.p.rapidapi.com/fitness';
     MapEntry<String, String> EE_Entry =
         MapEntry("totalDailyEnergyExpenditure.bmi.calories", "value");
+=======
+  void fetchBmi() async {
+    String baseURL = 'https://fitness-api.p.rapidapi.com/fitness';
+>>>>>>> 0a7a60136f2d78c788e6765098884e9d16dbfed2
 
     Map<String, String> requestHeaders = {
       'X-RapidAPI-Host': 'fitness-api.p.rapidapi.com',
-      'X-RapidAPI-Key': 'c93f0e349bmsh4f90f71e75907a8p166d42jsne574e84b3e47',
+      'X-RapidAPI-Key':
+          'c93f0e349bmsh4f90f71e75907a8p166d42jsne574e84b3e47', //TODO
       'Content-Type': 'application/x-www-form-urlencoded',
     };
 
     Map<String, String> payload = {
-      "bodyMassIndex": "conclusion, value",
-      "idealBodyWeight.peterson.metric": "value",
-      "bodyFatPercentage.bmi": "value",
-      EE_Entry.key: EE_Entry.value,
+      //TODO Add API request parameters
     };
 
     var request = http.Request('POST', Uri.parse(baseURL));
@@ -30,16 +31,19 @@ class ApiCalls {
     http.StreamedResponse response = await request.send();
 
     if (response.statusCode == 200) {
+<<<<<<< HEAD
       String ReceivedResponse = await response.stream.bytesToString();
       List<dynamic> jsonList = jsonDecode(ReceivedResponse) as List<dynamic>;
-
       List<Bmi> bmiInfo = jsonList.map((json) => Bmi.fromJson(json)).toList();
       return bmiInfo; //uncomment when apis are implemented in their respective pages
+      //TO DO return bmi data
+=======
       //TODO return Bmi object
+>>>>>>> 0a7a60136f2d78c788e6765098884e9d16dbfed2
     } else {
-      throw Exception('Failed to load bmi');
+      throw Exception('Failed to load bmi data.');
     }
   }
 
-  void fetchBurnedCalories() {} //still have yet to do
+  void fetchBurnedCalories() {}
 }
