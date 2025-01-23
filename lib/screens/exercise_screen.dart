feebcd2b069e25991667116a68e0f9eda0564925
juliusthecoder <julius.sim@live.com>
2025-01-24 00:48:31 +0800

    Some updated changes

--- lib/screens/exercise_screen.dart
+++ lib/screens/exercise_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fitness/models/exercise.dart';
 import 'package:fitness/utilities/firebase_calls.dart';
 import 'package:flutter/material.dart';
@@ -24,21 +25,63 @@ class _ExerciseScreenState extends State<ExerciseScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             //TODO StreamBuilder to show documents in exercises collection
-            // StreamBuilder<QuerySnapshot>(
-            //   stream: exercisesCollection
-            //       .where('userid', isEqualTo: auth.currentUser?.uid)
-            //       .snapshots(),
-            //   builder: (context, snapshot) {
-            //     if (snapshot.hasData) {
-            //       return Text(
-            //         '${snapshot.data?.docs.length} Tasks',
-            //         style: const TextStyle(color: Colors.white, fontSize: 18),
-            //       );
-            //     } else {
-            //       return const Center(child: CircularProgressIndicator());
-            //     }
-            //   },
-            // ),
+            StreamBuilder<QuerySnapshot>(
+              stream: exercisesCollection
+                  .where('userid', isEqualTo: auth.currentUser?.uid)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(
+                    '${snapshot.data?.docs.length} Exercises',
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+            Expanded(
+              child: Container(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: exercisesCollection
+                      .where('userid', isEqualTo: auth.currentUser?.uid)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          QueryDocumentSnapshot doc =
+                              snapshot.data!.docs[index];
+                          return ListTile(
+                            title: Text(
+                              doc['activity'],
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            subtitle: Text(
+                              doc['duration'],
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            trailing: Text(
+                              doc['burnedCalories'],
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
+              ),
+            ),
             ElevatedButton(
               onPressed: () {
                 showModalBottomSheet(
